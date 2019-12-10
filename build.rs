6db9f964b6a475537032f1dfae3d2fc1f0ecbd65
Nico Mandery <nico@nmandery.net>
2019-12-10 18:28:50 +0000

    initial commit

---
+++ build.rs
@@ -0,0 +1,50 @@
+extern crate bindgen;
+extern crate cmake;
+extern crate regex;
+
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+use cmake::Config;
+use regex::Regex;
+
+fn main() {
+    // build h3 as a static library
+    let dst_path = Config::new("libh3")
+        .define("ENABLE_COVERAGE", "OFF")
+        .define("BUILD_BENCHMARKS", "OFF")
+        .define("BUILD_FILTERS", "OFF")
+        .define("BUILD_GENERATORS", "OFF")
+        .build();
+
+
+    // link to the static library we just build
+    println!("cargo:rustc-link-lib=static=h3");
+    println!("cargo:rustc-link-search=native={}", dst_path.join("lib").display());
+
+    let header_path = dst_path.join("include/h3/h3api.h");
+    let mut builder = bindgen::Builder::default()
+        .header(dst_path.join("include/h3/h3api.h")
+            .into_os_string()
+            .into_string()
+            .expect("Path could not be converted to string"));
+
+    // read the contents of the header to extract functions and types
+    let header_contents = fs::read_to_string(header_path).expect("Unable to read h3 header");
+    for cap in Regex::new(r"H3_EXPORT\(\s*(?P<func>[a-zA-Z0-9_]+)\s*\)").unwrap().captures_iter(&header_contents) {
+        builder = builder.whitelist_function(&cap["func"]);
+    }
+    for cap in Regex::new(r"struct\s+\{[^\}]*}\s*(?P<type>[a-zA-Z0-9_]+)").unwrap().captures_iter(&header_contents) {
+        builder = builder.whitelist_type(&cap["type"]);
+    }
+    // Finish the builder and generate the bindings.
+    let bindings = builder.generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+mod test {
+    use *;
+    use std::os::raw::c_int;
+
+    #[test]
+    fn test_geo_to_h3() {
+        unsafe {
+            let input = GeoCoord {
+                lat: degsToRads(95.12345),
+                lon: degsToRads(-21.12345),
+            };
+            let res: c_int = 5;
+            let output = geoToH3(&input, res);
+            assert_eq!(output, 599073528557338623);
+            assert_eq!(format!("{:x}", output), "85056333fffffff")
+        }
+    }
+
+    #[test]
+    fn test_to_string() {
+        unsafe {
+            let index: H3Index = 599073528557338623;
+
+            let mut buf = vec![0u8; 17];
+            h3ToString(index, buf.as_mut_ptr() as *mut i8, buf.capacity());
+            assert_eq!(String::from_utf8(buf).unwrap().trim_end_matches('\0'), "85056333fffffff");
+        }
+    }
+}
