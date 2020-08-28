f7f071a902bd31079048e21a96fb99dc392e720c
Nico Mandery <nico@nmandery.net>
2020-08-28 13:31:57 +0200

    starting work on a new API

--- h3/src/lib.rs
+++ h3/src/lib.rs
@@ -14,18 +14,131 @@ use geo_types_06::{Coordinate, LineString, Point, Polygon};
 
 use h3_sys::{degsToRads, GeoCoord, Geofence, GeoPolygon, H3Index};
 use std::ffi::CString;
+use std::str::FromStr;
+use std::fmt;
 
 #[macro_use]
 mod util;
 pub mod stack;
 
-pub fn get_resolution(i: H3Index) -> i32 {
-    unsafe { h3_sys::h3GetResolution(i) }
+#[derive(Debug)]
+pub enum Error {
+    InvalidInput
 }
 
-pub fn get_parent(i: H3Index, resolution: i32) -> H3Index {
-    unsafe { h3_sys::h3ToParent(i, resolution as c_int) }
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "invalid string input for a H3 index")
+    }
 }
+
+impl std::error::Error for Error {}
+
+pub struct Index(H3Index);
+
+impl From<H3Index> for Index {
+    fn from(h3index: H3Index) -> Self {
+        Index(h3index)
+    }
+}
+
+impl Index {
+    pub fn resolution(&self) -> u8 {
+        (unsafe { h3_sys::h3GetResolution(self.0) }) as u8
+    }
+
+    pub fn is_valid(&self) -> bool {
+        unsafe { h3_sys::h3IsValid(self.0) != 0 }
+    }
+
+    pub fn get_parent(&self, resolution: u8) -> Index {
+        Index::from(unsafe { h3_sys::h3ToParent(self.0, resolution as c_int) })
+    }
+
+    pub fn polygon(&self) -> Option<Polygon<f64>> {
+        let gb = unsafe {
+            let mut mu = MaybeUninit::<h3_sys::GeoBoundary>::uninit();
+            h3_sys::h3ToGeoBoundary(self.0, mu.as_mut_ptr());
+            mu.assume_init()
+        };
+
+        if gb.numVerts > 0 {
+            let mut nodes = vec![];
+            for i in 0..gb.numVerts {
+                nodes.push((
+                    unsafe { h3_sys::radsToDegs(gb.verts[i as usize].lon) },
+                    unsafe { h3_sys::radsToDegs(gb.verts[i as usize].lat) },
+                ));
+            }
+            nodes.push((*nodes.first().unwrap()).clone());
+            Some(Polygon::new(LineString::from(nodes), vec![]))
+        } else {
+            None
+        }
+    }
+
+    pub fn coordinate(&self) -> Coordinate<f64> {
+        unsafe {
+            let mut gc = GeoCoord {
+                lat: 0.0,
+                lon: 0.0,
+            };
+            h3_sys::h3ToGeo(self.0, &mut gc);
+
+            Coordinate {
+                x: h3_sys::radsToDegs(gc.lon),
+                y: h3_sys::radsToDegs(gc.lat),
+            }
+        }
+    }
+
+    pub fn from_point(pt: &Point<f64>, h3_resolution: u8) -> Self {
+        let h3index = unsafe {
+            let gc = point_to_geocoord(pt);
+            h3_sys::geoToH3(&gc, h3_resolution as c_int)
+        };
+        Index::from(h3index)
+    }
+
+
+    pub fn from_coordinate(c: &Coordinate<f64>, h3_resolution: i32) -> Self {
+        let h3index = unsafe {
+            let gc = coordinate_to_geocoord(c);
+            h3_sys::geoToH3(&gc, h3_resolution as c_int)
+        };
+        Index::from(h3index)
+    }
+
+    pub fn k_ring(&self, k: i32) -> Vec<Index> {
+        let max_size = unsafe { h3_sys::maxKringSize(k) as usize };
+        let mut h3_indexes_out: Vec<H3Index> = vec![0; max_size];
+
+        unsafe {
+            h3_sys::kRing(self.0, k as c_int, h3_indexes_out.as_mut_ptr());
+        }
+        remove_zero_indexes_from_vec!(h3_indexes_out);
+        h3_indexes_out // TODO: convert to Index
+    }
+
+}
+
+impl ToString for Index {
+    fn to_string(&self) -> String {
+        format!("{:x}", self.0)
+    }
+}
+
+impl FromStr for Index {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let h3index: H3Index = CString::new(s).map(|cs| unsafe {
+            h3_sys::stringToH3(cs.as_ptr())
+        }).map_err(|_| Error::InvalidInput)?;
+        Ok(Index::from(h3index))
+    }
+}
+
 
 pub enum AreaUnits {
     M2,
@@ -113,56 +226,6 @@ pub fn polyfill_polygon(poly: &Polygon<f64>, h3_resolution: i32) -> Vec<H3Index>
     h3_indexes
 }
 
-pub fn point_to_h3index(pt: &Point<f64>, h3_resolution: i32) -> H3Index {
-    unsafe {
-        let gc = point_to_geocoord(pt);
-        h3_sys::geoToH3(&gc, h3_resolution as c_int)
-    }
-}
-
-pub fn polygon_from_h3index(h3index: H3Index) -> Option<Polygon<f64>> {
-    let gb = unsafe {
-        let mut mu = MaybeUninit::<h3_sys::GeoBoundary>::uninit();
-        h3_sys::h3ToGeoBoundary(h3index, mu.as_mut_ptr());
-        mu.assume_init()
-    };
-
-    if gb.numVerts > 0 {
-        let mut nodes = vec![];
-        for i in 0..gb.numVerts {
-            nodes.push((
-                unsafe { h3_sys::radsToDegs(gb.verts[i as usize].lon) },
-                unsafe { h3_sys::radsToDegs(gb.verts[i as usize].lat) },
-            ));
-        }
-        nodes.push((*nodes.first().unwrap()).clone());
-        Some(Polygon::new(LineString::from(nodes), vec![]))
-    } else {
-        None
-    }
-}
-
-pub fn coordinate_to_h3index(c: &Coordinate<f64>, h3_resolution: i32) -> H3Index {
-    unsafe {
-        let gc = coordinate_to_geocoord(c);
-        h3_sys::geoToH3(&gc, h3_resolution as c_int)
-    }
-}
-
-pub fn coordinate_from_h3index(h3index: H3Index) -> Coordinate<f64> {
-    unsafe {
-        let mut gc = GeoCoord {
-            lat: 0.0,
-            lon: 0.0,
-        };
-        h3_sys::h3ToGeo(h3index, &mut gc);
-
-        Coordinate {
-            x: h3_sys::radsToDegs(gc.lon),
-            y: h3_sys::radsToDegs(gc.lat),
-        }
-    }
-}
 
 /// group indexes by their resolution
 pub fn group_h3indexes_by_resolution(h3_indexes: &[H3Index]) -> HashMap<i32, Vec<H3Index>> {
@@ -175,31 +238,6 @@ pub fn group_h3indexes_by_resolution(h3_indexes: &[H3Index]) -> HashMap<i32, Vec
     m
 }
 
-pub fn k_ring(h3_index: H3Index, k: i32) -> Vec<H3Index> {
-    let max_size = unsafe { h3_sys::maxKringSize(k) as usize };
-    let mut h3_indexes_out: Vec<H3Index> = vec![0; max_size];
-
-    unsafe {
-        h3_sys::kRing(h3_index, k as c_int, h3_indexes_out.as_mut_ptr());
-    }
-    remove_zero_indexes_from_vec!(h3_indexes_out);
-    h3_indexes_out
-}
-
-
-pub fn h3_to_string(h3_index: H3Index) -> String {
-    format!("{:x}", h3_index)
-}
-
-pub fn string_to_h3(s: &str) -> Option<H3Index> {
-    CString::new(s).map(|cs| unsafe {
-        h3_sys::stringToH3(cs.as_ptr())
-    }).ok()
-}
-
-pub fn is_valid(h3_index: H3Index) -> bool {
-    unsafe { h3_sys::h3IsValid(h3_index) != 0 }
-}
 
 #[cfg(test)]
 mod tests {
