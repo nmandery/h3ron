3e9e7b2658487e75977687d2218f0292f2ae615c
Nico Mandery <nico@nmandery.net>
2020-12-23 16:51:48 +0100

    emptyarray error

--- h3-ndarray/src/algo.rs
+++ h3-ndarray/src/algo.rs
@@ -3,6 +3,7 @@ use geo_types::{Rect, Coordinate};
 use crate::transform::Transform;
 use crate::sphere::{area_rect, area_linearring};
 use h3::index::Index;
+use crate::error::Error;
 
 fn find_continous_chunks_along_axis<T>(a: &ArrayView2<T>, axis: usize, nodata_value: &T) -> Vec<(usize, usize)> where T: Sized + PartialEq {
     let mut chunks = Vec::new();
@@ -67,7 +68,10 @@ pub enum NearestH3ResolutionSearchMode {
 
 /// find the h3 resolution closed to the size of a pixel in an array
 /// of the given shape with the given transform
-pub fn nearest_h3_resolution(shape: &[usize; 2], transform: &Transform, search_mode: NearestH3ResolutionSearchMode) -> u8 {
+pub fn nearest_h3_resolution(shape: &[usize; 2], transform: &Transform, search_mode: NearestH3ResolutionSearchMode) -> Result<u8, Error> {
+    if shape[0] == 0 || shape[1] == 0 {
+        return Err(Error::EmptyArray);
+    }
     let bbox_array = Rect::new(
         transform * &Coordinate::from((0.0_f64, 0.0_f64)),
         transform * &Coordinate::from((
@@ -78,7 +82,7 @@ pub fn nearest_h3_resolution(shape: &[usize; 2], transform: &Transform, search_m
     let area_pixel = area_rect(&bbox_array) / (shape[0] * shape[1]) as f64;
     let center_array = bbox_array.center();
 
-    let mut closest_h3_res = 0;
+    let mut nearest_h3_res = 0;
     let mut area_difference = None;
     for h3_res in 0..=16 {
         // calculate the area of the center index to avoid using the approximate values
@@ -89,7 +93,7 @@ pub fn nearest_h3_resolution(shape: &[usize; 2], transform: &Transform, search_m
 
         match search_mode {
             NearestH3ResolutionSearchMode::IndexAreaSmallerThanPixelArea => if area_h3_index <= area_pixel {
-                closest_h3_res = h3_res;
+                nearest_h3_res = h3_res;
                 break;
             }
 
@@ -101,7 +105,7 @@ pub fn nearest_h3_resolution(shape: &[usize; 2], transform: &Transform, search_m
                 };
                 if let Some(old_area_difference) = area_difference {
                     if old_area_difference < new_area_difference {
-                        closest_h3_res = h3_res - 1;
+                        nearest_h3_res = h3_res - 1;
                         break;
                     } else {
                         area_difference = Some(new_area_difference);
@@ -113,7 +117,7 @@ pub fn nearest_h3_resolution(shape: &[usize; 2], transform: &Transform, search_m
         }
     }
 
-    closest_h3_res
+    Ok(nearest_h3_res)
 }
 
 #[cfg(test)]
@@ -165,10 +169,10 @@ mod tests {
         let gt = Transform::from_rasterio(&[
             0.0011965049999999992, 0.0, 8.11377, 0.0, -0.001215135, 49.40792
         ]);
-        let h3_res1 = nearest_h3_resolution(&[2000_usize, 2000_usize], &gt, NearestH3ResolutionSearchMode::SmallestAreaDifference);
+        let h3_res1 = nearest_h3_resolution(&[2000_usize, 2000_usize], &gt, NearestH3ResolutionSearchMode::SmallestAreaDifference).unwrap();
         assert_eq!(h3_res1, 10); // TODO: validate
 
-        let h3_res2 = nearest_h3_resolution(&[2000_usize, 2000_usize], &gt, NearestH3ResolutionSearchMode::IndexAreaSmallerThanPixelArea);
+        let h3_res2 = nearest_h3_resolution(&[2000_usize, 2000_usize], &gt, NearestH3ResolutionSearchMode::IndexAreaSmallerThanPixelArea).unwrap();
         assert_eq!(h3_res2, 11); // TODO: validate
     }
 }
--- h3-ndarray/src/error.rs
+++ h3-ndarray/src/error.rs
@@ -3,12 +3,14 @@ use std::fmt;
 #[derive(Debug)]
 pub enum Error {
     TransformNotInvertible,
+    EmptyArray,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::TransformNotInvertible => write!(f, "transform is not invertible"),
+            Self::EmptyArray => write!(f, "empty array"),
         }
     }
 }
