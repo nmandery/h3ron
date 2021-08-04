7377ae8199aaccd20e37b482bfe364c77e838a5e
Nico Mandery <nico@nmandery.net>
2021-08-04 19:23:34 +0200

    fix comments

--- h3ron-h3-sys/src/lib.rs
+++ h3ron-h3-sys/src/lib.rs
@@ -1,3 +1,8 @@
+//! low-level bindings to H3
+//!
+//! This crate includes the C sources for libh3, so compiling it requires a c-compiler and the `cmake`
+//! build tool.
+//!
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
@@ -6,10 +11,5 @@
 // https://github.com/nmandery/h3ron/issues/19
 // https://github.com/rust-lang/rust-bindgen/issues/1651
 #[allow(deref_nullptr)]
-
-//! low-level bindings to H3
-//!
-//! This crate includes the C sources for libh3, so compiling it requires a c-compiler and the `cmake`
-//! build tool.
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
