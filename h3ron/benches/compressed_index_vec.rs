c9f21143d29c0bac9bff20e7b231493c236b7b22
Nico Mandery <nico@nmandery.net>
2022-01-06 10:44:36 +0100

    benchmark for CompressedIndexVec

---
+++ h3ron/benches/compressed_index_vec.rs
@@ -0,0 +1,39 @@
+use std::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use geo_types::Coordinate;
+use h3ron::collections::compressed::CompressedIndexVec;
+
+use h3ron::H3Cell;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let cells = H3Cell::from_coordinate(&Coordinate::from((12.3, 45.4)), 10)
+        .unwrap()
+        .k_ring(200)
+        .iter()
+        .collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("compressed_index_vec");
+    group.sample_size(20);
+    group.warm_up_time(Duration::from_secs(1));
+    group.bench_function(format!("compress {} cells", cells.len()), |bencher| {
+        bencher.iter(|| {
+            let _cv = CompressedIndexVec::from(cells.clone());
+        });
+    });
+
+    let cv = CompressedIndexVec::from(cells.clone());
+    dbg!((cv.size_of_data_uncompressed(), cv.size_of_data_compressed()));
+    group.bench_function(
+        format!("decompress {} cells to vec", cells.len()),
+        |bencher| {
+            bencher.iter(|| {
+                let _cells2 = cv.to_vec();
+            });
+        },
+    );
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
